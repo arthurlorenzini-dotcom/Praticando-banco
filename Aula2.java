01d1f2f2cf5d02c5207fc4ec70ade576b1c07133
arthurlorenzini-dotcom <arthur.lorenzini@escola.pr.gov.br>
2026-09-15 14:54:27 +0000

    .

---
+++ Aula2.java
@@ -0,0 +1,9 @@
+public class Aula2 {
+    public static void main(String[] args) {
+        String[] contatosDaAna = {"Pai: 41999990000" , "Mãe: 41988880000"};
+
+        for (String contato : contatosDaAna) {
+        System.out.println(contato);
+        }
+    }
+}
